84bf8b30e91208815cf22eb59f19e7654b8e418a
faizhermawan_ <faizhermawan13@gmail.com>
2022-06-24 04:10:08 +0700

    add login and register page

---
+++ application/controllers/Auth.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function login()
+	{
+		$this->load->view('pages/v_login');
+	}
+
+	public function signup()
+	{
+		$this->load->view('pages/v_signup');
+	}
+}
